e266e605532c9d996083e596f12c665d7fd1ebae
Muhammad-nazriel <nazrielhilman259@gmail.com>
2025-01-12 00:05:55 +0700

    Add Procfile, requirements.txt, and runtime.txt

--- venv/app.py
+++ venv/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+import os
+import logging
 
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,4 +22,5 @@ def cart():
     return render_template('cart.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
